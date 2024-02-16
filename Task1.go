a3ac2b7487ecd2ac7ffb6b1a11a5fb1884fa2d43
IlyasNartay <ilyas.nartay@gmail.com>
2024-02-16 17:58:08 +0600

    SortSlice

--- Task1.go
+++ Task1.go
@@ -8,10 +8,10 @@ import (
 func main() {
 	a := [7]int{6, 3, 5, 6, 3, 5, 7}
 	b := a[2:7]
-	SortSlice(b)
+	SortSlice(&b)
 	fmt.Print(b)
 }
 
-func SortSlice(slice []int) {
-	sort.Slice(slice)
+func SortSlice(slice *[]int) {
+	sort.Slice(*slice, func(i, j int) bool { return i < j })
 }
